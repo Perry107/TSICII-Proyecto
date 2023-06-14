d85d61f303cdc0a01732adffeba20291da95d95e
HannCM <hscm.280400@gmail.com>
2023-06-14 17:54:37 -0600

    Nueva versión de la página

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
     <header class="bg-dark py-5">
         <div class="container px-4 px-lg-5 my-5">
             <div class="text-center text-white">
-                <h1 class="display-4 fw-bolder">HS</h1>
+                <h1 class="display-4 fw-bolder">The Legend</h1>
                 <p class="lead fw-normal text-white-50 mb-0">All for you concert!</p>
             </div>
         </div>
